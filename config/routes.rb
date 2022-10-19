985c6f5591b616ceacf13709455c34d2d9d1783b
VINICIOS PERUZZO AZAMBUJA <vinicios_azambuja@estudante.sc.senai.br>
2022-10-18 22:00:51 -0300

    aula 18.10

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'login/index'
   resources :types
   resources :bagmons
-  resources :users
+  root to: 'bagmons#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
