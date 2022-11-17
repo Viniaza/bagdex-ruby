f2dfd5d8586ef3ff1ee92986eeb4595d5decf3fe
VINICIOS PERUZZO AZAMBUJA <vinicios_azambuja@estudante.sc.senai.br>
2022-11-16 21:43:50 -0300

    battle front melhoria mostrar um pouco por vez

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( login.css )
 Rails.application.config.assets.precompile += %w( cadastro.css )
+Rails.application.config.assets.precompile += %w( forgot.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
