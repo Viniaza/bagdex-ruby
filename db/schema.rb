d1720f5cdb59b52874a3a44e6936b60faa7ae64b
Vinicios <vinicios.pazambuja@gmail.com>
2022-11-20 00:25:09 -0300

    login

--- db/schema.rb
+++ db/schema.rb
@@ -30,12 +30,4 @@ ActiveRecord::Schema.define(version: 2022_10_11_231645) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
